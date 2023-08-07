02d682156b7b4100fe0bc511bac3e3923ba27386
kimsihyoung <rjswmtlgud@naver.com>
2023-08-07 16:00:36 +0900

    initial

---
+++ src/main/java/com/test/Repository/practice/RepositoryPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.test.Repository.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryPracticeApplication.class, args);
+	}
+
+}
